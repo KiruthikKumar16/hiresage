2a50e30966eba4ca0b731f04c96a07a1675af9b0
Kiruthik Kumar M <m.kiruthikkumar@gmail.com>
2025-07-28 22:39:00 +0530

    Fix deployment error: Replace react-toastify with sonner in auth-provider

--- components/auth-provider.tsx
+++ components/auth-provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { createContext, useContext, useEffect, useState } from 'react'
-import { toast } from 'react-toastify'
+import { toast } from 'sonner'
 
 interface User {
   id: string
